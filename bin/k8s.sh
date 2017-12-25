fd926bbe8dd71bdf9ee806ae3094b9b55c85746d
Ankit Pokhrel <ankitpokhrel@gmail.com>
2017-12-25 19:57:28 +0700

    Use recursive syntax and create dir if not exist

--- bin/k8s.sh
+++ bin/k8s.sh
@@ -11,10 +11,10 @@ docker build --no-cache -t tus-php-client-k8s -f Dockerfile.client.k8s .
 docker build --no-cache -t tus-php-server-k8s -f Dockerfile.server.k8s .
 
 # Delete old resources
-kubectl delete -f k8s/
+kubectl delete -f k8s/ --recursive=true
 
 # Create resources
-kubectl create -f k8s/
+kubectl create -f k8s/ --recursive=true
 
 # Serve client in browser
 minikube service tus-php-client
