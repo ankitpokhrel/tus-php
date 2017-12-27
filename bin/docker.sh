9bccc41db763b1186c0710b5112cf91272891cfd
Ankit Pokhrel <ankitpokhrel@gmail.com>
2017-12-28 00:31:02 +0700

    Create uploads dir if it doesn't exist

--- bin/docker.sh
+++ bin/docker.sh
@@ -17,3 +17,6 @@ COMPOSE_FILE="docker/docker-compose.yml"
 
 docker-compose -p tus-php -f ${COMPOSE_FILE} down
 docker-compose -p tus-php -f ${COMPOSE_FILE} up --build --remove-orphans -d
+
+# Create uploads dir
+mkdir -p uploads
