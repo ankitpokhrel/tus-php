6bea37620a7840b335e4cb92c646a9ce4c5be5b2
Felipe Fidelix <felipe@fdx.is>
2019-01-05 13:10:44 +0000

    Fixed tests for #120

--- tests/Tus/ServerTest.php
+++ tests/Tus/ServerTest.php
@@ -1224,6 +1224,17 @@ class ServerTest extends TestCase
             ], true)
             ->andReturn(true);
 
+        $fileMock
+            ->shouldReceive('setChecksum')
+            ->once()
+            ->with($checksum)
+            ->andReturnSelf();
+
+        $fileMock
+            ->shouldReceive('setKey')
+            ->once()
+            ->andReturnSelf();
+
         $response = $this->tusServerMock->handleConcatenation($fileName, $filePath . $fileName);
 
         $this->assertEquals(201, $response->getStatusCode());
@@ -1332,6 +1343,11 @@ class ServerTest extends TestCase
             ->once()
             ->with($files)
             ->andReturn(30);
+
+        $fileMock
+            ->shouldReceive('setKey')
+            ->once()
+            ->andReturnSelf();
 
         $response = $this->tusServerMock->handleConcatenation($fileName, $filePath . $fileName);
 
