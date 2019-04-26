18ef3b94c44af1b09c1d3496bbd4ad912f5e3c0e
Ankit Pokhrel <ankitpokhrel@gmail.com>
2019-04-26 22:21:12 +0700

    Fix verify method

--- example/basic/verify.php
+++ example/basic/verify.php
@@ -12,41 +12,27 @@ use GuzzleHttp\Exception\ConnectException;
 
 $client = new \TusPhp\Tus\Client('http://tus-php-server');
 
-// Alert: Sanitize all inputs properly in production code
-if ( ! empty($_FILES)) {
-    $status    = 'new';
-    $fileMeta  = $_FILES['tus_file'];
-    $uploadKey = hash_file('md5', $fileMeta['tmp_name']);
+$uploadKey = uniqid('tus_file_');
 
-    try {
-        $offset = $client->setKey($uploadKey)->file($fileMeta['tmp_name'])->getOffset();
+try {
+    $offset = $client->setKey($uploadKey)->getOffset();
+    $status = false !== $offset ? 'resume' : 'new';
+    $offset = false === $offset ? 0 : $offset;
 
-        if (false !== $offset) {
-            $status = $offset >= $fileMeta['size'] ? 'uploaded' : 'resume';
-        } else {
-            $offset = 0;
-        }
-
-        echo json_encode([
-            'status' => $status,
-            'bytes_uploaded' => $offset,
-            'upload_key' => $uploadKey,
-        ]);
-    } catch (ConnectException $e) {
-        echo json_encode([
-            'status' => 'error',
-            'bytes_uploaded' => -1,
-        ]);
-    } catch (FileException $e) {
-        echo json_encode([
-            'status' => 'resume',
-            'bytes_uploaded' => 0,
-            'upload_key' => '',
-        ]);
-    }
-} else {
+    echo json_encode([
+        'status' => $status,
+        'bytes_uploaded' => $offset,
+        'upload_key' => $uploadKey,
+    ]);
+} catch (ConnectException $e) {
     echo json_encode([
         'status' => 'error',
         'bytes_uploaded' => -1,
+    ]);
+} catch (FileException $e) {
+    echo json_encode([
+        'status' => 'resume',
+        'bytes_uploaded' => 0,
+        'upload_key' => '',
     ]);
 }
