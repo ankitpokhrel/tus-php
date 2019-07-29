4755607cf83719ab56e13ba1335ba7e28505f780
Paweł Niedzielski <pawel.tadeusz.niedzielski@gmail.com>
2019-07-29 16:12:14 +0200

    Fix tests

--- tests/FileTest.php
+++ tests/FileTest.php
@@ -491,7 +491,7 @@ class FileTest extends TestCase
      * @covers ::merge
      *
      * @expectedException \TusPhp\Exception\FileException
-     * @expectedExceptionMessageRegExp /Cannot copy source \(\S+\) to destination \(\S+\)\./
+     * @expectedExceptionMessageRegExp /Cannot copy source \(.+\) to destination \(.+\)\./
      */
     public function it_throws_file_exception_if_it_cannot_copy_file()
     {
