0b626b7a0d26b9a7969f1722e0a446f120d2b1ac
Samundra Shrestha <samundra.shr@gmail.com>
2019-08-16 00:06:29 +0700

    move configs to server and client

--- bin/rebuild.sh
+++ bin/rebuild.sh
@@ -1,2 +1,6 @@
-docker stop tus-php-server tus-php-client tus-php-base tus-php-base tus-php-redis
-docker rm tus-php-server tus-php-client tus-php-base tus-php-base tus-php-redis
+echo 'Stop and remove tus-php containers ...'
+docker stop tus-php-server tus-php-client tus-php-base tus-php-base tus-php-redis >> /dev/null
+docker rm tus-php-server tus-php-client tus-php-base tus-php-base tus-php-redis >> /dev/null
+
+echo 'Remove tus-php docker images ...'
+docker rmi tusphp_tus-server tusphp_tus-client tus-php-base >> /dev/null
