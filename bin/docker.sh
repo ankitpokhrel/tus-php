942380e1059200e1a7147a0784c121e89f53946c
Samundra Shrestha <samundra.shr@gmail.com>
2019-08-18 06:28:15 +0700

    change upload folder mode
    - change upload folder ownership to www-data
    - remove commented code in default.conf

--- bin/docker.sh
+++ bin/docker.sh
@@ -21,4 +21,4 @@ docker-compose -p tus-php -f ${COMPOSE_FILE} down
 docker-compose -p tus-php -f ${COMPOSE_FILE} up --build --remove-orphans -d
 
 mkdir -p uploads
-chmod 777 uploads
+chown -R www-data:www-data uploads
