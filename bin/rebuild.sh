ca304166840baa28283add69e65a8cf0dc81e342
Samundra Shrestha <samundra.shr@gmail.com>
2019-08-18 21:18:43 +0700

    do not run tus-php-base continer

--- bin/rebuild.sh
+++ bin/rebuild.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 echo 'Stop and remove tus-php containers ...'
-docker stop tus-php-server tus-php-client tus-php-base tus-php-base tus-php-redis >> /dev/null
-docker rm tus-php-server tus-php-client tus-php-base tus-php-base tus-php-redis >> /dev/null
+docker stop tus-php-server tus-php-client tus-php-redis >> /dev/null
+docker rm tus-php-server tus-php-client tus-php-redis >> /dev/null
 
 echo 'Remove tus-php docker images ...'
-docker rmi tusphp-tus-server tusphp-tus-client tus-php-base >> /dev/null
+docker rmi tus-php_tus-server tus-php_tus-client tus-php-base >> /dev/null
