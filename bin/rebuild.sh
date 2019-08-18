b0825f28fa060e1e62447ad6d67ad81a71357119
Samundra Shrestha <samundra.shr@gmail.com>
2019-08-18 18:52:52 +0700

    move php.ini, www.conf to base image

--- bin/rebuild.sh
+++ bin/rebuild.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
+
 echo 'Stop and remove tus-php containers ...'
 docker stop tus-php-server tus-php-client tus-php-base tus-php-base tus-php-redis >> /dev/null
 docker rm tus-php-server tus-php-client tus-php-base tus-php-base tus-php-redis >> /dev/null
 
 echo 'Remove tus-php docker images ...'
-docker rmi tusphp_tus-server tusphp_tus-client tus-php-base >> /dev/null
+docker rmi tusphp-tus-server tusphp-tus-client tus-php-base >> /dev/null
