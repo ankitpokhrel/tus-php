72178396381aa04f748a76457fd956367e33083f
Ankit Pokhrel <ankitpokhrel@gmail.com>
2020-11-05 21:59:17 +0100

    dev: Use official php docker image (#311)

--- docker/server/entrypoint.sh
+++ docker/server/entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
+
+# Install dependencies.
 composer install
 
-# This /sbin/runit-wrapper has been copied from the base image phpearth/php:7.2-nginx
-# This is necessary to keep container in running state, otherwise container will
-# exit immediately after it's built. We need to keep it running to handle
-# incoming request. /sbin/runit-wrapper also handles SIGINT properly and
-# let system handle those signals appropriately.
-# see https://github.com/phpearth/docker-php/blob/master/docker/tags/nginx/sbin/runit-wrapper
-/sbin/runit-wrapper
+# Start supervisord.
+/usr/bin/supervisord -n -c /etc/supervisord.conf
