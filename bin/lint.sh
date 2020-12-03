1d61d8f0e9fac8a50e91f365edf478db8c284f85
Ankit Pokhrel <ankitpokhrel@gmail.com>
2020-12-03 20:09:31 +0100

    def: Fix linter (#328)

---
+++ bin/lint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+FIXER="php-cs-fixer"
+bin=/usr/local/bin/${FIXER}
+
+if ! command -v php-cs-fixer &>/dev/null; then
+    wget https://cs.symfony.com/download/php-cs-fixer-v2.phar -O ${bin} && chmod a+x ${bin}
+fi
+
+if [[ "$1" == "dry" ]]; then
+    ${FIXER} fix --diff-format=udiff --dry-run --show-progress=dots --using-cache=no --verbose
+else
+    ${FIXER} fix
+fi
