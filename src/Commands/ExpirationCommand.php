14bd9602c23ad72efd4d89d506f113cb69d7c622
Jon Gotlin <jon@jon.se>
2024-01-01 17:43:43 +0100

    dep: Allow Symfony 7 (#433)

--- src/Commands/ExpirationCommand.php
+++ src/Commands/ExpirationCommand.php
@@ -41,7 +41,7 @@ class ExpirationCommand extends Command
     /**
      * {@inheritDoc}
      */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $output->writeln([
             '<info>Cleaning server resources</info>',
@@ -71,6 +71,6 @@ class ExpirationCommand extends Command
 
         $output->writeln('');
 
-        return 0;
+        return Command::SUCCESS;
     }
 }
