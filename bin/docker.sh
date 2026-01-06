52ff45b5f7fd1f4c1301ae490aae266fbd5aa989
Dennis Haseloff <dennis.haseloff@alemo.de>
2026-01-06 09:20:14 +0100

    Upgrade to Symfony 8 and PHP8.4

--- bin/docker.sh
+++ bin/docker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-PHP_VERSION=${PHP_VERSION-7}
 ROOT_DIR=${ROOT_DIR-$(pwd)}
 BASE_PATH="${ROOT_DIR}/docker/base/"
 DOCKER_FILENAME="${BASE_PATH}Dockerfile"
@@ -11,16 +10,12 @@ NETWORK_NAME="tus-php-network"
 (docker network ls -f name=${NETWORK_NAME} | grep ${NETWORK_NAME}) 2> /dev/null || \
     docker network create ${NETWORK_NAME}
 
-# Build base image.
-if [[ ${PHP_VERSION} == "8" ]]; then
-    docker build -t tus-php-base -f ${DOCKER_FILENAME}.php8 ${BASE_PATH}
-else
-    docker build -t tus-php-base -f ${DOCKER_FILENAME} ${BASE_PATH}
-fi
+
+docker build -t tus-php-base -f ${DOCKER_FILENAME} ${BASE_PATH}
 
 # Build client and server.
-docker-compose -p tus-php -f ${COMPOSE_FILE} down
-docker-compose -p tus-php -f ${COMPOSE_FILE} up --build --remove-orphans -d
+docker compose -p tus-php -f ${COMPOSE_FILE} down
+docker compose -p tus-php -f ${COMPOSE_FILE} up --build --remove-orphans -d
 
 docker exec tus-php-server mkdir -p uploads
 docker exec tus-php-server chown www-data:root -R uploads
